55b9a556ec940cb8ac0e54ca270d65eb62123a55
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-04-27 00:24:01 +0100

    update socials

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -15,8 +15,8 @@ export const MENU_LINKS = [
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/chrismwilliams/astro-cactus",
-	twitter: "https://github.com/chrismwilliams/astro-cactus",
-	discord: "https://github.com/chrismwilliams/astro-cactus",
-	email: "me@example.com",
+	github: "https://github.com/cawnj",
+	linkedin: "https://www.linkedin.com/in/conor-joyce-00/",
+	discord: "https://discord.com/users/179307238601654273",
+	email: "conor@cawnj.dev",
 };
